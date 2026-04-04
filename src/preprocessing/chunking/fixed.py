619d93d427be7ed7eb95948c9aaaecb85794c63a
Dan <asign1022@gmail.com>
2026-04-05 00:45:44 +0800

    feat: create fixed chunking stragety

---
+++ src/preprocessing/chunking/fixed.py
@@ -0,0 +1,34 @@
+import uuid
+
+from langchain_text_splitters import TokenTextSplitter
+
+from ingestion.loaders.base import RawDocument
+from preprocessing.chunking.base import BaseChunker, ChunkConfig, ChunkedDocument
+
+
+class FixedChunker(BaseChunker):
+    def __init__(self, config: ChunkConfig):
+        super().__init__(config)
+        self.splitter = TokenTextSplitter(
+            chunk_size=self.config.chunk_size,
+            chunk_overlap=self.config.chunk_overlap,
+        )
+
+    def chunk(self, doc: RawDocument) -> ChunkedDocument:
+        chunks = self.splitter.split_text(doc.text)
+        return [
+            ChunkedDocument(
+                chunk_id=str(uuid.uuid4()),
+                text=chunk,
+                # Approximation, can change into tiktoken
+                token_count=len(chunk.split()),
+                metadata={
+                    **doc.metadata,
+                    "title": doc.title,
+                    "source_type": doc.source_type.value,
+                    "chunk_index": idx,
+                    "total_chunks": len(chunks),
+                },
+            )
+            for idx, chunk in enumerate(chunks)
+        ]
