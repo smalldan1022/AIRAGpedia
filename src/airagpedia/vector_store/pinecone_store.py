2578375b52d44d56e0d7f3ae3c3287c6fe5ca715
Dan <asign1022@gmail.com>
2026-05-02 18:50:53 +0800

    feat(vector_store): update the pinecone methods

--- src/airagpedia/vector_store/pinecone_store.py
+++ src/airagpedia/vector_store/pinecone_store.py
@@ -1,5 +1,6 @@
 import os
 import time
+from typing import Any
 
 from pinecone import Pinecone, ServerlessSpec
 
@@ -16,8 +17,7 @@ class PineconeConfig(BaseVectorStoreConfig):
 class PineconeVectorStore(BaseVectorStore):
     def __init__(self, config: PineconeConfig):
         super().__init__(config)
-        self.pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-        self.load()
+        self._connect = False
 
     def _create_index(self) -> None:
         if self.config.index_name not in self.pc.list_indexes().names():
@@ -28,26 +28,42 @@ class PineconeVectorStore(BaseVectorStore):
                 spec=ServerlessSpec(cloud="aws", region="us-east-1"),
             )
 
-    def load(self) -> None:
+    def connect(self) -> None:
+        self.pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
         self._create_index()
         self.index = self.pc.Index(self.config.index_name)
+        self.index_stats = self.index.describe_index_stats()
+        self._connect = True
 
-    def add(self, vectors, metadata, batch_size=100):
-        assert len(vectors) == len(metadata)
+    def disconnect(self) -> None:
+        self.pc = None
+        self.index = None
+        self.index_stats = None
+        self._connect = False
+
+    def add(
+        self,
+        ids: list[str],
+        vectors: list[float],
+        metadata: list[dict],
+        batch_size: int = 100,
+    ):
+        if not self._connect:
+            raise RuntimeError("Not connected. Call connect() first.")
+        assert len(vectors) == len(ids) == len(metadata), (
+            "The data lengths are not consistent."
+        )
 
         dim = len(vectors[0])
-        assert all(len(vector) == dim for vector in vectors)
+        assert all(len(vector) == dim for vector in vectors), (
+            "The dimensions are not consistent."
+        )
 
         # (id_, emb, metadata)
-        vectors_to_db = list(
-            zip([meta["chunk_id"] for meta in metadata], vectors, metadata)
-        )
+        vectors_to_db = list(zip(ids, vectors, metadata))
 
         for i in range(0, len(vectors_to_db), batch_size):
             self.index.upsert(vectors=vectors_to_db[i : i + batch_size])
-
-    def save(self):
-        pass
 
     def search(self, query: list[float], top_k: int = 3) -> list[dict]:
         results = self.index.query(vector=query, top_k=top_k, include_metadata=True)
@@ -56,8 +72,26 @@ class PineconeVectorStore(BaseVectorStore):
             for match in results.matches
         ]
 
-    def delete(self):
-        pass
+    @property
+    def index_count(self) -> int:
+        return self.index_stats.total_vector_count
 
-    def update(self):
+    def delete(
+        self,
+        ids: list[str] | None = None,
+        filter: dict[str, Any] | None = None,
+        delete_all: bool = False,
+    ) -> int:
+        if delete_all:
+            self.index.delete(delete_all=True)
+            return self.index_count  # Return previous count
+
+        elif ids:
+            self.index.delete(ids=ids)
+            return len(ids)
+        elif filter:
+            self.index.delete(filter=filter)
+            return -1
+
+    def upsert(self, ids: list[str], vectors: list[list[float]], metadata: list[dict]):
         pass
